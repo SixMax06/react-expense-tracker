c7a06984dc4db2c119144508613fff5ea54c100e
SixMax06 <alemilamax@gmail.com>
2025-11-26 11:50:28 +0100

    aggiunto l'elemento "Expense"
    fetch funziona correttamente

--- react-expense-tracker/src/App.jsx
+++ react-expense-tracker/src/App.jsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import PocketBase from 'pocketbase'
 import './App.css'
 import Graph from './react-components/Graph'
+import Expense from './react-components/Expense.jsx'
 
-function App() {
+const makeFetch = async() => {
+  const db = new PocketBase('http://127.0.0.1:8090')
+  let records = await db.collection('expenses').getFullList()
+  records = records.map(record => ({id: record.id, title: record.title, description: record.description, price: record.price, category: record.category}))
+  console.log(records)
+  return records
+}
 
+const App = () => {
+
+  let [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const records = await makeFetch()
+      setData(records)
+      console.log(records)
+    }
+    fetchData()
+  }, [])
 
   return (
     <>
-      <Graph/>
+      <div className="App">
+        {data.map(element => {
+          return (<Expense 
+            key={element.id}
+            att_ID={element.id} 
+            att_title={element.title} 
+            att_description={element.description} 
+            att_price={element.price} 
+            att_category={element.category} 
+          />)
+        })}
+      </div>
     </>
   )
 }
---
+++ react-expense-tracker/src/react-components/Expense.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import '../App.css'
+
+const Expense = ({att_ID, att_title, att_description, att_price, att_category}) => {
+    const [ID, setID] = useState(att_ID)
+    const [title, setTitle] = useState(att_title)
+    const [description, setDescription] = useState(att_description)
+    const [price, setPrice] = useState(att_price)
+    const [category, setCategory] = useState(att_category)
+
+    return (
+        <div className="expense-card">
+            <h2>{title}</h2>
+            <h3>{description}</h3>
+            <p>Price: ${price}</p>
+            <p>Category: {category}</p>
+        </div>
+    )
+}
+
+export default Expense
